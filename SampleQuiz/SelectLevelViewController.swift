06e72227aad503ea46d3979d22b3e1b4aa2c2a61
HelloApp <stogashi146@gmail.com>
2021-09-19 12:18:53 +0900

    [add] ・難易度選択画面 ・難易度選択でコンソールに出力 ・タイトルから難易度画面に遷移

---
+++ SampleQuiz/SelectLevelViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SelectLevelViewController.swift
+//  SampleQuiz
+//
+//  Created by 富樫駿 on 2021/09/19.
+//
+
+import UIKit
+
+class SelectLevelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func leveButtonAction(sender: UIButton) {
+        print(sender.tag)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
